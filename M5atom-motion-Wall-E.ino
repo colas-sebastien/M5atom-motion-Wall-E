dca79ca7883a6eeb42360815c27badf5c027a273
colas-sebastien <colas.sebastien@free.fr>
2025-01-30 17:34:40 +0100

    First Release

---
+++ M5atom-motion-Wall-E.ino
@@ -0,0 +1,244 @@
+/*
+*******************************************************************************
+*
+*
+*
+*******************************************************************************
+ */
+
+#include <M5Atom.h>
+#include "AtomMotion.h"
+#include "Grove_Motor_Driver_TB6612FNG.h"
+#include "Ultrasonic.h"
+#include <Wire.h>
+#include <Bluepad32.h>
+#include <Adafruit_NeoPixel.h>
+
+#define PIN_RGB              27  
+#define NUMPIXELS             1   
+#define GAMEPAD_DEAD_ZONE_X  50
+#define GAMEPAD_DEAD_ZONE_Y  50
+#define GAMEPAD_MAX_X       510
+#define GAMEPAD_MAX_Y       510
+#define MOTOR_MIN_SPEED      50
+#define MOTOR_MAX_SPEED     255
+
+/*
+ * PORT A => PORT_SDA 26
+ *           PORT_SCL 32
+ *
+ * For Atom Motion
+ * PORT B => PORT_SDA 33
+ *           PORT_SDA 23
+ * PORT C => PORT_SDA 19
+ *           PORT_SDA 22
+ * I2C    => PORT_SDA 25
+ *        => PORT_SCL 21 
+ */
+// PORT B for Motor Cotroller
+#define PORT_SDA                  33
+#define PORT_SCL                  23
+
+// PORT C for UktraSonicSensor
+#define ULTRASONIC_PORT           22
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel( NUMPIXELS, PIN_RGB,NEO_GRB + NEO_KHZ800);
+
+ControllerPtr myControllers[BP32_MAX_CONTROLLERS];
+MotorDriver motor;
+
+AtomMotion Atom;
+bool direction = true;
+Ultrasonic ultrasonic(ULTRASONIC_PORT);
+long RangeInCentimeters;
+
+void GetStatus() {
+    for (int ch = 1; ch < 5; ch++) {
+        Serial.printf("Servo Channel %d: %d \n", ch, Atom.ReadServoAngle(ch));
+    }
+    Serial.printf("Motor Channel %d: %d \n", 1, Atom.ReadMotorSpeed(1));
+    Serial.printf("Motor Channel %d: %d \n", 2, Atom.ReadMotorSpeed(2));
+}
+
+void setup() {
+
+  M5.begin(true,false);
+  Atom.Init();
+  pixels.begin();
+  pixels.setPixelColor(0,0,0,200);
+  pixels.show();
+  Wire.begin(PORT_SDA, PORT_SCL);
+  Serial.begin(115200);
+  while (!Serial) {
+    ;
+  }  
+
+  Serial.println();
+  Serial.println("M5ZR");
+
+  String fv = BP32.firmwareVersion();
+  Serial.print("Firmware version installed: ");
+  Serial.println(fv);
+
+  // To get the BD Address (MAC address) call:
+  const uint8_t* addr = BP32.localBdAddress();
+  Serial.print("BD Address: ");
+  for (int i = 0; i < 6; i++) {
+    Serial.print(addr[i], HEX);
+    if (i < 5)
+      Serial.print(":");
+    else
+      Serial.println();
+  }
+
+  motor.init();  
+
+  BP32.setup(&onConnectedController, &onDisconnectedController);
+  BP32.forgetBluetoothKeys();  
+}
+
+void onConnectedController(ControllerPtr ctl) {
+  bool foundEmptySlot = false;
+
+  pixels.setPixelColor(0,0,50,0);
+  pixels.show();
+
+  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+    if (myControllers[i] == nullptr) {
+      Serial.print("CALLBACK: Controller is connected, index=");
+      Serial.println(i);
+      myControllers[i] = ctl;
+      foundEmptySlot = true;
+
+      // Optional, once the gamepad is connected, request further info about the
+      // gamepad.
+      ControllerProperties properties = ctl->getProperties();
+      char buf[80];
+      sprintf(buf,
+              "BTAddr: %02x:%02x:%02x:%02x:%02x:%02x, VID/PID: %04x:%04x, "
+              "flags: 0x%02x",
+              properties.btaddr[0], properties.btaddr[1], properties.btaddr[2],
+              properties.btaddr[3], properties.btaddr[4], properties.btaddr[5],
+              properties.vendor_id, properties.product_id, properties.flags);
+      Serial.println(buf);
+      break;
+    }
+  }
+  if (!foundEmptySlot) {
+    Serial.println(
+        "CALLBACK: Controller connected, but could not found empty slot");
+  }
+}
+
+void onDisconnectedController(ControllerPtr ctl) {
+  bool foundGamepad = false;
+  
+  pixels.setPixelColor(0,100,0,0);
+  pixels.show();
+
+  for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
+    if (myControllers[i] == ctl) {
+      Serial.print("CALLBACK: Controller is disconnected from index=");
+      Serial.println(i);
+      myControllers[i] = nullptr;
+      foundGamepad = true;
+      break;
+    }
+  }
+
+  if (!foundGamepad) {
+    Serial.println(
+        "CALLBACK: Controller disconnected, but not found in myControllers");
+  }
+}
+
+void processGamepad(ControllerPtr gamepad) {
+  int16_t speed_x;
+  int16_t speed_y;
+  // There are different ways to query whether a button is pressed.
+  // By query each button individually:
+  //  a(), b(), x(), y(), l1(), etc...
+
+  if (gamepad->a()) {   
+    Atom.SetServoAngle(1, 0);
+    Atom.SetServoAngle(3, 180);
+    // A pressed
+  }
+
+  if (gamepad->b()) {
+    Atom.SetServoAngle(1, 180);
+    Atom.SetServoAngle(3, 0);
+    //B pressed
+  }
+
+  if (gamepad->x())  
+  {
+    // X pressed
+    Atom.SetServoAngle(1, 90);
+    Atom.SetServoAngle(2, 90);
+    Atom.SetServoAngle(3, 90);
+  }
+
+  if (gamepad->y())  
+  {
+    // Y pressed
+  }
+  speed_x=MOTOR_MIN_SPEED+(abs(gamepad->axisX())-GAMEPAD_DEAD_ZONE_X)*MOTOR_MAX_SPEED/GAMEPAD_MAX_X;
+  speed_y=MOTOR_MIN_SPEED+(abs(gamepad->axisY())-GAMEPAD_DEAD_ZONE_Y)*MOTOR_MAX_SPEED/GAMEPAD_MAX_Y;
+
+  if (gamepad->axisY()<-GAMEPAD_DEAD_ZONE_Y) {          // FORWARD
+    if (gamepad->axisX()<-200) {                        //  + LEFT
+      motor.dcMotorRun(MOTOR_CHA, -speed_y);
+      motor.dcMotorRun(MOTOR_CHB, -(speed_y-speed_x+MOTOR_MIN_SPEED));
+    }
+    else if (gamepad->axisX()>200) {                    //  + RIGHT
+      motor.dcMotorRun(MOTOR_CHA, -(speed_y-speed_x+MOTOR_MIN_SPEED));
+      motor.dcMotorRun(MOTOR_CHB, -speed_y);
+    }
+    else
+    {
+      motor.dcMotorRun(MOTOR_CHA, -speed_y);
+      motor.dcMotorRun(MOTOR_CHB, -speed_y);    
+    }
+  }
+  else if (gamepad->axisY()>GAMEPAD_DEAD_ZONE_Y) {      // BACKWARD    
+    motor.dcMotorRun(MOTOR_CHA,  speed_y);
+    motor.dcMotorRun(MOTOR_CHB,  speed_y);
+  }
+  else  if (gamepad->axisX()<-GAMEPAD_DEAD_ZONE_X) {    // LEFT
+    motor.dcMotorRun(MOTOR_CHA, -150);
+    motor.dcMotorRun(MOTOR_CHB,  150);
+  }
+  else   if (gamepad->axisX()>GAMEPAD_DEAD_ZONE_Y) {    // RIGHT
+    motor.dcMotorRun(MOTOR_CHA,  150);
+    motor.dcMotorRun(MOTOR_CHB, -150);
+  } 
+  else {                                // STOP
+    motor.dcMotorStop(MOTOR_CHA);
+    motor.dcMotorStop(MOTOR_CHB);
+  }
+}
+
+
+
+void loop()
+{
+  BP32.update();
+
+  // It is safe to always do this before using the controller API.
+  // This guarantees that the controller is valid and connected.
+  for (int i = 0; i < BP32_MAX_CONTROLLERS; i++) {
+    ControllerPtr myController = myControllers[i];
+
+    if (myController && myController->isConnected()) {
+      if (myController->isGamepad())
+        processGamepad(myController);
+    }
+  }
+
+  RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
+  //Serial.print(RangeInCentimeters);//0~400cm
+  //Serial.println(" cm");
+
+  delay(50);
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# M5atom-motion-Wall-E
-DIY Wall-E
+![M5ZR](img/wall-e-01.jpg)
+
+# M5Atom Wall-E
+
+Create your Wall-E thanks to M5atom
+
+## Hardware
+- [1x Zumo Chassis Kit](https://www.pololu.com/product/1418) 
+- [2x Micro Metal Gearmotor HP 6V](https://www.pololu.com/product/1101) 
+- [Grove - I2C Motor Driver](https://www.seeedstudio.com/Grove-I2C-Motor-Driver-TB6612FNG-p-3220.html) 
+- [M5Atom](https://shop.m5stack.com/products/atomic-motion-base-stm32f030?variant=44393917219073) 
+- [Atomic Motion Base](https://shop.m5stack.com/products/atom-tailbat) 
+- [Battery 16340 2600mAh](https://www.amazon.fr/Rechargeables-Batterie-2800mAh-Capacit%C3%A9-Frontale/dp/B0CTQ4X244)
+- [Battery holder for 16340](https://www.amazon.fr/dp/B07MC7WR8J?ref=ppx_yo2ov_dt_b_fed_asin_title)
+- [3x ServoMotors sg90](https://www.amazon.fr/dp/B0DFCGXY2J)
+- [Grove Ultrasonic distance sensor](https://www.seeedstudio.com/Grove-Ultrasonic-Distance-Sensor.html)
+- [A bluetooth gamepad supported by BluePad32 for Arduino](https://gitlab.com/ricardoquesada/bluepad32-arduino) 
+
+
+
+
+![zumo](img/zumo.jpg)
+![motor](img/motor.jpg)
+![I2C motor driver](img/i2c_motor.jpg)
+![M5Atom lite](img/m5atom.jpg)
+![Atomic Motion Base](img/atomic-motion.jpg)
+![Battery holder for 16340](img/support_battery.jpg)
+![Battery 16340 2600mAh](img/16340.jpg)
+![Servo Motor](img/servomotor.jpg)
+![Grove Ultrasonic distance sensor](img/grove-ultrasonic-distance-sensor.jpg)
+![Steam Controller](img/steam-controller.jpg)
+
+## Software
