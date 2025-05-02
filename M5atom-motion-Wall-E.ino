742de23052b2e7b84582240d8722d3d0f85af460
colas-sebastien <colas.sebastien@free.fr>
2025-05-02 18:52:19 +0200

    Version 1 (without ultrasonic)

--- M5atom-motion-Wall-E.ino
+++ M5atom-motion-Wall-E.ino
@@ -1,18 +1,21 @@
 /*
-*******************************************************************************
-*
-*
-*
-*******************************************************************************
+
+    M5 Atom WALL-E Project
+
+    Create your own WALL-E thanks to M5 Atom / M5 Atom Echo
+
+    Author: Sébastien Colas
+
  */
 
 #include <M5Atom.h>
-#include "AtomMotion.h"
-#include "Grove_Motor_Driver_TB6612FNG.h"
-#include "Ultrasonic.h"
+#include <SPIFFS.h>
+#include <Grove_Motor_Driver_TB6612FNG.h>
+#include <Ultrasonic.h>
 #include <Wire.h>
 #include <Bluepad32.h>
 #include <Adafruit_NeoPixel.h>
+#include "AtomMotion.h"
 
 #define PIN_RGB                 27  
 #define NUMPIXELS                1   
@@ -22,10 +25,22 @@
 #define GAMEPAD_MAX_Y          510
 #define GAMEPAD_MAX_BRAKE     1020
 #define GAMEPAD_MAX_THROTTLE  1020
+#define GAMEPAD_BUTTON_X         0
+#define GAMEPAD_BUTTON_Y         1
+#define GAMEPAD_BUTTON_A         2
+#define GAMEPAD_BUTTON_B         3
+#define GAMEPAD_BUTTON_L1        4
+#define GAMEPAD_BUTTON_R1        5
+#define GAMEPAD_BUTTON_PREV      6
+#define GAMEPAD_BUTTON_NEXT      7
+#define GAMEPAD_BUTTON_LOGO      8
+
 #define MOTOR_MIN_SPEED         50
 #define MOTOR_MAX_SPEED        255
 #define MOTOR_A_DIR              1
 #define MOTOR_B_DIR             -1
+
+#define SMOOTH_NUMBER           20
 
 /*
  * PORT A => PORT_SDA 26
@@ -56,18 +71,73 @@ bool direction = true;
 Ultrasonic ultrasonic(ULTRASONIC_PORT);
 long RangeInCentimeters;
 
-void GetStatus() {
-    for (int ch = 1; ch < 5; ch++) {
+bool    buttons[9] = {false, false, false, false, false, false, false, false, false};
+//      Buttons         X      Y      A      B     L1     R1     PREV   NEXT   LOGO
+
+int32_t hands_values[2][SMOOTH_NUMBER];
+int8_t  hands_index=0;
+
+int32_t smooth_value(int32_t *values)
+{
+  int32_t value =    0;
+  int32_t min   = 1000;
+  int32_t max   =    0;
+  int32_t sum   =    0;
+
+  for (int32_t i=0;i<SMOOTH_NUMBER;i++)
+  {
+    if (values[i]<min)
+    {
+      min=values[i];
+    }
+    if (values[i]>max)
+    {
+      max=values[i];
+    }
+    sum+=values[i]; 
+  }
+  return round((sum-min-max)/(SMOOTH_NUMBER-2));
+}
+
+bool button_pressed(bool state,uint8_t id)
+{
+  bool pressed=false;
+  if (state)                      // Button is currently pressed
+  {                               
+    if (buttons[id])              // Button is still pressed 
+    {
+                                  // Nothing to to
+    }
+    else                          // Button was previously  not pressed
+    {
+      buttons[id]=true;
+      pressed=true;   
+    }    
+  }
+  else
+  {
+    buttons[id]=false;
+  }
+  return pressed;
+}
+
+void GetServoStatus() 
+{
+    for (int ch = 1; ch < 4; ch++) 
+    {
         Serial.printf("Servo Channel %d: %d \n", ch, Atom.ReadServoAngle(ch));
     }
-    Serial.printf("Motor Channel %d: %d \n", 1, Atom.ReadMotorSpeed(1));
-    Serial.printf("Motor Channel %d: %d \n", 2, Atom.ReadMotorSpeed(2));
 }
 
 void setup() {
 
+  for (int i=0;i<SMOOTH_NUMBER;i++)
+  {
+    hands_values[0][i]=0;
+    hands_values[1][i]=0;
+  }
   M5.begin(true,false);
-  Atom.Init();
+  Atom.Init();  
   pixels.begin();
   pixels.setPixelColor(0,0,0,200);
   pixels.show();
@@ -98,7 +168,7 @@ void setup() {
   motor.init();  
 
   BP32.setup(&onConnectedController, &onDisconnectedController);
-  BP32.forgetBluetoothKeys();  
+  BP32.forgetBluetoothKeys();       
 }
 
 void onConnectedController(ControllerPtr ctl) {
@@ -156,43 +226,50 @@ void onDisconnectedController(ControllerPtr ctl) {
   }
 }
 
-bool    head_pos = true;
-bool    hand_manual = false;
+bool    head_pos      = true;
+bool    hand_manual   = false;
+bool    hand_left_up  = false;
+bool    hand_right_up = false;
+
 
 void processGamepad(ControllerPtr gamepad) {
   
   int16_t speed_x;
   int16_t speed_y;
-  // There are different ways to query whether a button is pressed.
-  // By query each button individually:
-  //  a(), b(), x(), y(), l1(), etc...
 
   if (hand_manual)
+  { 
+    hands_values[0][hands_index]=gamepad->brake();
+    hands_values[1][hands_index]=gamepad->throttle();
+
+    hands_index=(hands_index+1)%SMOOTH_NUMBER;    
+    
+    Atom.SetServoAngle(1, 180-smooth_value(hands_values[0])*120/GAMEPAD_MAX_BRAKE);        // Left Hand
+    Atom.SetServoAngle(3, 0  +smooth_value(hands_values[1])*120/GAMEPAD_MAX_THROTTLE);     // Right Hand
+  }
+
+  if (button_pressed(gamepad->a(),GAMEPAD_BUTTON_A))                // A pressed
+  {                                                                 // Hands down + Head front
+    Atom.SetServoAngle(1, 180);                                     // Left Hand
+    Atom.SetServoAngle(3, 0  );                                     // Right Hand
+    Atom.SetServoAngle(2, 90);                                      // Head Front
+  }
+
+  if (button_pressed(gamepad->b(),GAMEPAD_BUTTON_B))                // B pressed
+  {                         
+    hand_manual=!hand_manual;                                       // Activating hand movement control by triggers
+  }
+
+  if (button_pressed(gamepad->x(),GAMEPAD_BUTTON_X))                // X pressed
   {
-    Atom.SetServoAngle(1, 180-gamepad->brake()*120/GAMEPAD_MAX_BRAKE);           // Left Hand
-    Atom.SetServoAngle(3, 0  +gamepad->throttle()*120/GAMEPAD_MAX_THROTTLE);     // Right Hand
+    Atom.SetServoAngle(1,   0);                                     // Left Hand Rear
+    Atom.SetServoAngle(3, 180);                                     // Right Hand Rear
+    Atom.SetServoAngle(2, 90);                                      // Head Front           
   }
 
-  if (gamepad->a())                 // A pressed
-  {                                 // Hands down
-    Atom.SetServoAngle(1, 180);     // Left Hand
-    Atom.SetServoAngle(3, 0  );     // Right Hand
-  }
-
-  if (gamepad->b())               // B pressed
-  {                               // Hands up
-    hand_manual=!hand_manual;
-    delay(100);
-  }
-
-  if (gamepad->x())  
+  if (button_pressed(gamepad->y(),GAMEPAD_BUTTON_Y))               // Y pressed
   {
-    // X pressed
-    Atom.SetServoAngle(2, 90);    
-  }
-
-  if (gamepad->y())               // Y pressed
-  {
+    head_pos=!head_pos;
     if (head_pos)
     {
       Atom.SetServoAngle(2, 110);
@@ -201,9 +278,59 @@ void processGamepad(ControllerPtr gamepad) {
     {
       Atom.SetServoAngle(2, 70);
     }
-    head_pos=!head_pos;
-    delay(100);
   }
+
+  if (button_pressed(gamepad->l1(),GAMEPAD_BUTTON_L1))              // Left Button pressed
+  {                               
+      hand_left_up=!hand_left_up;                                   // Move up or down the left hand
+      hand_manual=false;                                            // Take over manual control
+      if (hand_left_up)
+      {
+        Atom.SetServoAngle(1, 180-120);
+      }
+      else
+      {
+        Atom.SetServoAngle(1, 180);
+      }
+  }
+
+  if (button_pressed(gamepad->r1(),GAMEPAD_BUTTON_R1))              // Rigth Button pressed
+  {                               
+      hand_right_up=!hand_right_up;                                 // Move up or down the left hand
+      hand_manual=false;                                            // Take over manual control
+      if (hand_right_up)
+      {
+        Atom.SetServoAngle(3, 0+120);
+      }
+      else
+      {
+        Atom.SetServoAngle(3, 0);
+      }
+  }
+
+  if (button_pressed(gamepad->miscBack(),GAMEPAD_BUTTON_PREV))     // Left Button pressed
+  {                               
+      hand_left_up=true;                                            // Move up the left hand up
+      hand_manual=false;                                            // Take over manual control
+      Atom.SetServoAngle(1, 180-120);                               // Left hand up
+      hand_right_up=false;                                          // Move the right hand down
+      Atom.SetServoAngle(3, 0);                                     // Right hand down
+      head_pos=false;                                               // Move Head to the right
+      Atom.SetServoAngle(2, 70);                                    // Head to the right
+
+  }
+
+  if (button_pressed(gamepad->miscHome(),GAMEPAD_BUTTON_NEXT))      // Rigth Button pressed
+  {                               
+      hand_left_up=false;                                           // Move up the left hand down
+      hand_manual=false;                                            // Take over manual control
+      Atom.SetServoAngle(1, 180);                                   // Left hand down
+      hand_right_up=true;                                           // Move the right hand down
+      Atom.SetServoAngle(3, 0+120);                                 // Right hand up
+      head_pos=true;                                                // Move Head to the right
+      Atom.SetServoAngle(2, 110);                                   // Head to the right
+  }  
+
 
   speed_x=MOTOR_MIN_SPEED+(abs(gamepad->axisX())-GAMEPAD_DEAD_ZONE_X)*MOTOR_MAX_SPEED/GAMEPAD_MAX_X;
   speed_y=MOTOR_MIN_SPEED+(abs(gamepad->axisY())-GAMEPAD_DEAD_ZONE_Y)*MOTOR_MAX_SPEED/GAMEPAD_MAX_Y;
@@ -239,9 +366,6 @@ void processGamepad(ControllerPtr gamepad) {
     motor.dcMotorStop(MOTOR_CHA);
     motor.dcMotorStop(MOTOR_CHB);
   }
-
-  // Left trigger Serial.println(gamepad->brake());
-  //Serial.println(gamepad->throttle());
 }
 
 
@@ -264,5 +388,6 @@ void loop()
   // RangeInCentimeters = ultrasonic.MeasureInCentimeters(); // two measurements should keep an interval
   // Serial.print(RangeInCentimeters);//0~400cm
   // Serial.println(" cm");  
-  delay(50);
+
+  delay(10);
 }
